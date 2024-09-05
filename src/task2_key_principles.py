83058789bd16ed48f8c75f091e9b588192d27fa4
mehalyna <firstmy555@gmail.com>
2024-09-05 20:21:38 +0300

    update task2

--- src/task2_key_principles.py
+++ src/task2_key_principles.py
@@ -11,12 +11,15 @@ def create_scatter_plot(data):
     Parameters:
     data (DataFrame): A DataFrame containing 'x' and 'y' columns.
     """
+    fig, _ = plt.subplots()
     sns.scatterplot(x='x', y='y', data=data)
     plt.title('Scatter Plot of X vs Y')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.grid(True)
     plt.show()
+    return fig
+
 
 
 # Example data
--- tests/test_task2.py
+++ tests/test_task2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from src.task2_key_principles import create_scatter_plot
 
@@ -6,7 +7,9 @@ from src.task2_key_principles import create_scatter_plot
 def test_create_scatter_plot():
     df = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
     try:
-        create_scatter_plot(df)
+        fig = create_scatter_plot(df)
+        # Check if the figure and axes are created
+        assert isinstance(fig, plt.Figure), "Failed to create a Matplotlib figure."
         print("Scatter plot generated successfully.")
     except Exception as e:
         assert False, f"Scatter plot generation failed with error: {e}"
